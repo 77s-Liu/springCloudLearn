064dbc853e51dedafabcb9dc37dea9eaa86d004b
77s-Liu <15524470270@163.com>
2018-12-20 13:53:35 +0800

    博客服务

--- BlogsViewer_Contents/src/main/java/com/springcloud/learn/blog_viewer_content/BlogViewerContentApplication.java
+++ BlogsViewer_Contents/src/main/java/com/springcloud/learn/blog_viewer_content/BlogViewerContentApplication.java
@@ -1,20 +1,46 @@
 package com.springcloud.learn.blog_viewer_content;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @MapperScan("com.springcloud.learn.blog_viewer_content.mapper")
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class BlogViewerContentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogViewerContentApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 }
 
